9d3e8543c8413aea24303fef05052df1126b4285
Robert Lee <bobbydwise1@hotmail.com>
2019-07-19 15:32:21 -0700

    Add problem text.

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Daily Coding Problem 2019-07-19
 
-####  _Good morning! Here's your coding interview problem for today.
+####  _ Good morning! Here's your coding interview problem for today._
 
-This problem was asked by Google.
+_This problem was asked by Facebook._
 
-An XOR linked list is a more memory efficient doubly linked list. Instead of each node holding next and prev fields, it holds a field named both, which is an XOR of the next node and the previous node. Implement an XOR linked list; it has an add(element) which adds the element to the end, and a get(index) which returns the node at index.
+_Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded._
 
-If using a language that has no pointers (such as Python), you can assume you have access to get_pointer and dereference_pointer functions that converts between nodes and memory addresses._
+_For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'._
+
+_You can assume that the messages are decodable. For example, '001' is not allowed._
 
 * _Note: problem has been completed in javascript._
--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
 /*
-This problem was asked by Google.
+Good morning! Here's your coding interview problem for today.
 
-An XOR linked list is a more memory efficient doubly linked list. Instead of each node holding next and prev fields, it holds a field named both, which is an XOR of the next node and the previous node. Implement an XOR linked list; it has an add(element) which adds the element to the end, and a get(index) which returns the node at index.
+This problem was asked by Facebook.
 
-If using a language that has no pointers (such as Python), you can assume you have access to get_pointer and dereference_pointer functions that converts between nodes and memory addresses.
+Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.
+
+For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
+
+You can assume that the messages are decodable. For example, '001' is not allowed.
 */
-
-//Note, this is done in javascript.
-
-//XOR: f(0,0)=false, f(0,1)=true, f(1,0)=true, f(1,1)=false
 
 const makeRandomNumber = () => {
   return Math.ceil(Math.random()*10);
